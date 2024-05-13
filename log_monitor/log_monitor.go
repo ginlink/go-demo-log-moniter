3dcaa94299f2e2893b599e2053f2a11d63491cfb
ginlink <ginlink1998@gmail.com>
2024-05-13 22:43:38 +0800

    feat: add read and write

--- log_monitor/log_monitor.go
+++ log_monitor/log_monitor.go
@@ -1,53 +1,95 @@
 package log_monitor
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
 	"strings"
 	"time"
 )
 
 // ----- 抽象层 -----
 type Reader interface {
-	Read(c chan string)
+	Read(rc chan []byte)
 }
 type Writer interface {
-	Write(c chan string)
+	Write(wc chan string)
 }
 
 // ----- 实现层 -----
 type LogMonitor struct {
-	rc     chan string
+	rc     chan []byte
 	wc     chan string
 	reader Reader
 	writer Writer
 }
 
-type MonitorReader struct{}
-
-func (m *MonitorReader) Read(c chan string) {
-	s := "message"
-	c <- s
+func (l *LogMonitor) Parse() {
+	for s := range l.rc {
+		str := strings.ToUpper(string(s))
+		l.wc <- str
+	}
 }
 
-type MonitorWriter struct{}
+type MonitorReader struct {
+	path string
+}
+
+func (m *MonitorReader) Read(rc chan []byte) {
+	f, err := os.Open(m.path)
+	if err != nil {
+		panic(fmt.Sprintf("open file err: %s", err.Error()))
+	}
+
+	// 偏移 从末尾读取
+	f.Seek(0, 2)
+	rd := bufio.NewReader(f)
+
+	for {
+		line, err := rd.ReadBytes('\n')
+		if err == io.EOF {
+			time.Sleep(500 * time.Millisecond)
+			continue
+		} else if err != nil {
+			panic(fmt.Sprintf("ReadBytes err: %s", err.Error()))
+		}
+
+		rc <- line
+	}
+}
+
+type MonitorWriter struct {
+	path string
+}
 
 func (m *MonitorWriter) Write(c chan string) {
-	fmt.Println(<-c)
-}
+	f, err := os.OpenFile(m.path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(fmt.Sprintf("open file err: %s", err.Error()))
+	}
+	defer f.Close()
 
-func (l *LogMonitor) Parse() {
-	s := <-l.rc
-	s = strings.ToUpper(s)
-	l.wc <- s
+	for str := range c {
+		_, err := f.WriteString(str)
+		if err != nil {
+			fmt.Printf("write err: %s", err.Error())
+			continue
+		}
+	}
 }
 
 // ----- 业务逻辑层 -----
 func Run() {
-	rc := make(chan string)
+	rc := make(chan []byte)
 	wc := make(chan string)
 
-	reader := &MonitorReader{}
-	writer := &MonitorWriter{}
+	reader := &MonitorReader{
+		path: "./access.log",
+	}
+	writer := &MonitorWriter{
+		path: "./output.log",
+	}
 
 	l := &LogMonitor{
 		rc:     rc,
@@ -60,5 +102,7 @@ func Run() {
 	go l.Parse()
 	go l.writer.Write(l.wc)
 
-	time.Sleep(1 * time.Second)
+	for {
+		time.Sleep(time.Second) // 阻塞主线程
+	}
 }
